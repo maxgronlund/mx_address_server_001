dd80ae75ee2295baad94756095486440ba156ab2
Max Grønlund <max@synthmax.dk>
2018-07-26 15:31:36 +0200

    more peer types

--- app/models/mx_peer.rb
+++ app/models/mx_peer.rb
@@ -3,12 +3,15 @@ class MxPeer < ApplicationRecord
   AUTHORIZATION_PROVIDER='authorization_provider'
   AUTHENTICATION_PROVIDER='authentication_provider'
   PUBLIC_LEDGER='public_ledger'
+  PAYMENT_GATEWAY='payment_gateway'
+  THIRD_PARTY_SERVICE='third_party_service'
 
   SERVER_TYPES =
     [
       ['Address server', ADDRESS_SERVER],
       ['Authorization provider', AUTHORIZATION_PROVIDER],
       ['Authentication provider', AUTHENTICATION_PROVIDER],
-      ['Public ledger', PUBLIC_LEDGER]
+      ['Payment gateway', PAYMENT_GATEWAY],
+      ['Third party service', THIRD_PARTY_SERVICE]
     ].freeze
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module MxAddressServer001
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.uuid =  "5546db05-f9a2-40d5-a1a1-4c84ec72aecd"
+    config.site_name =  "Address server 001"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
